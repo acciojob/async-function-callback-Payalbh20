cadbd40dbb415e76cf95f93221554fba2132bc61
Payalbh20 <117173906+Payalbh20@users.noreply.github.com>
2023-03-31 18:33:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-const url = "https://jsonplaceholder.typicode.com/posts/1";
+sync function getData(callback) {
+		  const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+		  const data = await response.json();
+		  callback(data.title);
+		}
 
-//your JS code here. If required.
+		document.getElementById('btn').addEventListener('click', async () => {
+			getData((title) => {
+				document.getElementById('output').innerText = title;
+			});
+		});
