7abdbd9c03e8e2ebdd3930e55404e952a5f4ed2b
Payalbh20 <117173906+Payalbh20@users.noreply.github.com>
2023-04-09 11:34:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-sync function getData(callback) {
-		  const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
-		  const data = await response.json();
-		  callback(data.title);
-		}
-
-		document.getElementById('btn').addEventListener('click', async () => {
-			getData((title) => {
-				document.getElementById('output').innerText = title;
-			});
-		});
+async function fetchData(callback) {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+    const data = await response.json();
+    callback(data);
+  }
+  
+  function displayMessage(data) {
+      document.getElementById("output").innerText = data.title;
+  }
